354fefbca8d09a52999fac9b94a9dd29871d08b7
sm.hassan90 <sm.hassan90@gmail.com>
2023-02-23 12:49:10 +0500

    redirection

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,8 @@
 const AllConstant = {
     //baseURL: "http://172.16.16.196:8080",
  //   baseURL: "http://192.168.100.80:8080",
-    baseURL: "http://192.168.0.52:8080/tci_server",
+//    baseURL: "http://192.168.0.52:8080/tci_server",
+    baseURL: "https://hawk.greenstar.org.pk:8441/tci_server",
     baseString: "encryptTest",
     timeout: "999999",
     base_upload_url: "https://hawk.greenstar.org.pk:81/uploads/"
